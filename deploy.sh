54aee0590d810e5ad2e0d33331974ecbc6c8a5a6
malshanucsc <randhajapvt@gmail.com>
2023-12-22 12:52:20 +0530

    updated buildspec and deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
-aws cloudfront create-invalidation
+#! /bin/bash
+
+apt install jq
+COMMIT_ID=$(cat version.json | jq -r '.commitId')
+echo $COMMIT_ID
+aws cloudfront create-invalidation --distribution-id

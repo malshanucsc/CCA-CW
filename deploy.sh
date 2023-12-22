d39750bcc688442505bbec1550f2acd2ed7d7d43
malshanucsc <randhajapvt@gmail.com>
2023-12-22 13:01:46 +0530

    distribution id added

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 apt install jq
 COMMIT_ID=$(cat version.json | jq -r '.commitId')
 echo $COMMIT_ID
-aws cloudfront create-invalidation --distribution-id
+aws cloudfront create-invalidation --distribution-id E2VN6ABMEOQ0CQ

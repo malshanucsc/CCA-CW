44ca4297fc58f27d1a9c297752bfa176193cc727
malshanucsc <randhajapvt@gmail.com>
2023-12-22 13:10:13 +0530

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@
 apt install jq
 COMMIT_ID=$(cat version.json | jq -r '.commitId')
 echo $COMMIT_ID
-aws cloudfront create-invalidation --distribution-id E2VN6ABMEOQ0CQ
+INVALIDATION_JSON="{ \"DistributionId\": \"E2VN6ABMEOQ0CQ\",\"InvalidationBatch\": {\"Paths\": {\"Quantity\": 1,\"Items\": [ \"/*\"]},\"CallerReference\": \"$COMMIT_ID\"}}"
+echo $INVALIDATION_JSON
+aws cloudfront create-invalidation --cli-input-json "$INVALIDATION_JSON"

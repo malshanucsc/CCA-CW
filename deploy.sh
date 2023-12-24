820f1f48883d33ed02281c61c2809f9ae4c4dcd6
malshanucsc <randhajapvt@gmail.com>
2023-12-24 10:45:51 +0530

    deploy file updated

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 apt install jq
+cat version.json
 COMMIT_ID=$(cat version.json | jq -r '.commitId')
 echo $COMMIT_ID
 INVALIDATION_JSON="{ \"DistributionId\": \"E2VN6ABMEOQ0CQ\",\"InvalidationBatch\": {\"Paths\": {\"Quantity\": 1,\"Items\": [ \"/*\"]},\"CallerReference\": \"$COMMIT_ID\"}}"
